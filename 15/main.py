52f7d7211e47be453d04e0f4ac2e4d9e0dd1b5e8
Sahan Fernando <sahan.h.fernando@gmail.com>
2020-12-16 12:25:31 +1100

    Optimized day 15 a bit

--- 15/main.py
+++ 15/main.py
@@ -3,26 +3,24 @@
 # https://github.com/SerenityOS/serenity/issues/4417
 # gcc is broken in serenityos pending a non-trivial fix, so we are doing today in python :/
 
+from time import sleep
 import re
 
 with open('input', 'r') as f:
     data = f.read()
 
 arr = [int(x) for x in data.split(',')]
-seen = {x: i for (i, x) in enumerate(arr[:-1])}
 
 def solve(index):
-    while len(arr) < index:
-        if (len(arr) % 10000) == 0:
-            print('Progress:', len(arr), '/', index)
-        x = arr[-1]
-        if x in seen:
-            v = len(arr) - seen[x] - 1
-        else:
-            v = 0
-        seen[x] = len(arr) - 1
-        arr.append(v)
-    return arr[index-1]
+    if index < len(arr):
+        return arr[index]
+    x = arr[-1]
+    seen = {x: i for (i, x) in enumerate(arr[:-1])}
+    for pos in range(len(arr), index):
+        v = pos - seen[x] - 1 if x in seen else 0
+        seen[x] = pos - 1
+        x = v
+    return x
 
 print("Part 1:", solve(2020))
 print("Part 2:", solve(30000000))
